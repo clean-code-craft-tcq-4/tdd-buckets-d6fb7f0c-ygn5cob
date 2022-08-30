80c575081fedca39cd29708318b6db81e36ffb28
Gnanasekaran Yuvaraja (MS/ECR4-XC) <ygn5cob@bosch.com>
2022-08-30 11:34:08 +0530

    a2d solving error-4

--- test_current_ranges.cpp
+++ test_current_ranges.cpp
@@ -33,3 +33,32 @@ TEST_CASE("TEST_CASE 4") {
     REQUIRE(objectofranges.size() == 1);
     REQUIRE(objectofranges[0] == "1-1, 6");
 }
+
+TEST_CASE("A2D TEST_CASE 4")
+{
+    {
+        int a2dBitValues[] = {0,0,0,0, 0,0,0,0, 0,0,0,0};
+        int exptectedValue = 0;
+        REQUIRE(A2D_12BitArrayToValue(a2dBitValues) == exptectedValue);
+    }
+    {
+        int a2dBitValues[] = {0,0,0,0, 0,0,0,0, 0,0,0,1};
+        int exptectedValue = 1;
+        REQUIRE(A2D_12BitArrayToValue(a2dBitValues) == exptectedValue);
+    }
+    {
+        int a2dBitValues[] = {0,1,1,1, 1,1,0,1, 0,0,0,0};
+        int exptectedValue = 2000;
+        REQUIRE(A2D_12BitArrayToValue(a2dBitValues) == exptectedValue);
+    }
+    {
+        int a2dBitValues[] = {1,1,1,1, 1,1,1,1, 1,1,1,0};
+        int exptectedValue = 4094;
+        REQUIRE(A2D_12BitArrayToValue(a2dBitValues) == exptectedValue);
+    }
+    {
+        int a2dBitValues[] = {1,1,1,1, 1,1,1,1, 1,1,1,1};
+        int exptectedValue = 4095;
+        REQUIRE(A2D_12BitArrayToValue(a2dBitValues) == exptectedValue);
+    }
+}
